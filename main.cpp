56fb28c757056cc07f9d09cf0d6c0f44ff80e2f4
UponTheSky <sailing.to.orbis@gmail.com>
2023-09-02 22:53:07 +0900

    ex-ch5-1

--- main.cpp
+++ main.cpp
@@ -88,35 +88,35 @@ int main()
   glDeleteShader(fragmentShader);
 
   // input data
-  // float vertices[] = {
-  //   -0.5f, -0.5f, 0.0f,
-  //   0.5f, -0.5f, 0.0f,
-  //   0.0f, 0.5f, 0.0f
-  // };
   float vertices[] = {
-    0.5f, 0.5f, 0.0f, // top right
-    0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f, 0.5f, 0.0f // top left
+    -0.5f, -0.5f, 0.0f,
+    0.0f, -0.5f, 0.0f,
+    -0.25f, 0.25f, 0.0f,
+    0.0f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.25f, 0.25f, 0.0f
   };
+  // float vertices[] = {
+  //   0.5f, 0.5f, 0.0f, // top right
+  //   0.5f, -0.5f, 0.0f, // bottom right
+  //   -0.5f, -0.5f, 0.0f, // bottom left
+  //   -0.5f, 0.5f, 0.0f // top left
+  // };
 
-  unsigned int indices[] = {
-    0, 1, 3, 1, 2, 3
-  };
-
-
-
+  // unsigned int indices[] = {
+  //   0, 1, 3, 1, 2, 3
+  // };
 
   unsigned int VAO, VBO;
-  unsigned int EBO;
+  // unsigned int EBO;
   glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &EBO);
+  // glGenBuffers(1, &EBO);
   glGenBuffers(1, &VBO);
 
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -135,8 +135,8 @@ int main()
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // check and call events, and swap the buffers
     glfwSwapBuffers(window);
