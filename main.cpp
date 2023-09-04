01ada439bb45f83a289048bb5d90ba61d304d38a
UponTheSky <sailing.to.orbis@gmail.com>
2023-09-04 22:56:56 +0900

    ex: ch8.1

--- main.cpp
+++ main.cpp
@@ -148,12 +148,12 @@ int main()
 
     ourShader.use();
 
-    // glm::mat4 trans = glm::mat4(1.0);
-    // trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0.0));
-    // trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+    glm::mat4 trans = glm::mat4(1.0);
+    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+    trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0.0));
 
-    // unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
 
     glBindVertexArray(VAO);
