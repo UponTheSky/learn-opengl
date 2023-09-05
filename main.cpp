2e2a71d6de7dc308b91b68bb5b648194e2beacb1
UponTheSky <sailing.to.orbis@gmail.com>
2023-09-05 15:03:38 +0900

    book: ch10 - revolving

--- main.cpp
+++ main.cpp
@@ -160,6 +160,14 @@ int main()
 
   glEnable(GL_DEPTH_TEST);
 
+  glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+  glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+  glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+
   while (!glfwWindowShouldClose(window)) {
     // input
     processInput(window);
@@ -178,13 +186,13 @@ int main()
     glBindVertexArray(VAO);
 
 
-    // glm::mat4 model = glm::mat4(1.0f);
-    // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    const float radius = 10.0f;
+    float camX = sin(glfwGetTime()) * radius;
+    float camZ = cos(glfwGetTime()) * radius;
+    glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
 
     ourShader.setMat4("view", view);
     ourShader.setMat4("projection", projection);
