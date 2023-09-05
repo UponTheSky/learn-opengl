40bb2cca80f222573fd5bdd3b0b12c58adda20a2
UponTheSky <sailing.to.orbis@gmail.com>
2023-09-05 14:38:01 +0900

    ex: ch9-3

--- main.cpp
+++ main.cpp
@@ -194,7 +194,11 @@ int main()
     for (unsigned int i = 0; i < 10; i++) {
       glm::mat4 model = glm::mat4(1.0f);
       model = glm::translate(model, cubePositions[i]);
-      model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+      if (i % 3 == 0) {
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+      } else {
+        model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
+      }
       ourShader.setMat4("model", model);
 
       glDrawArrays(GL_TRIANGLES, 0, 36);
